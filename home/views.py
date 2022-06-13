5e7e9f7800ae1d120efa6e1d3db7f7b0f528ba98
samadvalipour <samadvalipour76@gmail.com>
2022-06-13 23:03:08 +0430

    add search feature

--- home/views.py
+++ home/views.py
@@ -2,13 +2,15 @@ from django.shortcuts import render
 from .models import Post
 from django.views import View
 from account.forms import RegistrationForm,LoginForm
+from home.forms import SearchForm
 
 class HomeView(View):
     def get(self,request):
         registrationform = RegistrationForm()
         loginform = LoginForm()
+        searchform = SearchForm()
         all_posts = Post.objects.all()
-        return render(request,'home/index.html',context={'title':'home','registrationform':registrationform,'loginform':loginform,"posts":all_posts})
-   
-    def post(self,request):
-        return render(request,'home/index.html',context={"title":"home" })
+        search = request.GET.get("search")
+        if search:
+            all_posts = Post.objects.filter(body__icontains=search)
+        return render(request,'home/index.html',context={'title':'home','registrationform':registrationform,'loginform':loginform,"posts":all_posts,"is_homepage":True,"searchform":searchform})
